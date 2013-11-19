7f1a0175c7f092b565330c757ccc8a1720304f4a
Susan Brown <brownsusan@fullsail.edu>
2013-11-19 15:03:45 -0500

    update docs

--- listeners/document.js
+++ listeners/document.js
@@ -1,15 +1,38 @@
 module.exports.setup = function(socketServer, userSocket) {
 
+	var liveModel = require('../models/live');
+
 	userSocket.on('title_event_from_client', function(data) {
 		userSocket.broadcast.to(userSocket.room).emit('title_event_from_server', data);
 
 		//Update doc title
+		liveModel.readById(data.document_id, function(err, results) {
+			var doc = results;
+
+			doc.title = data.title;
+
+			liveModel.update(doc, function(err, results) {
+				console.log(err);
+			});
+
+		});
 	});
 
 	userSocket.on('body_event_from_client', function(data) {
 		userSocket.broadcast.to(userSocket.room).emit('body_event_from_server', data);
-		console.log(data);
+
 		//Update doc body
+		liveModel.readById(data.document_id, function(err, results) {
+			var doc = results;
+
+			doc.body = data.body;
+
+			liveModel.update(doc, function(err, results) {
+				console.log(err);
+			});
+
+		});
+
 	});
 
 }
