8f3855ce472cd177246f63413ddb74e947841504
Alan James <ajames@Landmine.fullsail.com>
2013-11-19 21:10:05 +0100

    Commit

--- models/live.js
+++ models/live.js
@@ -4,42 +4,36 @@ var mongoClient = mongodb.MongoClient;
 var databaseName = 'coactive';
 var collectionName = 'live';
 
+var database;
+
+mongoClient.connect('mongodb://127.0.0.1:27017/' + databaseName, function(err, db) {
+	database = db;
+});
+
 exports.create = function(document, fn) {
 
-	mongoClient.connect('mongodb://127.0.0.1:27017/' + databaseName, function(err, db) {
-
-		db.collection(collectionName).insert(document, function(err, results) {
-			fn(err, results);
-		});
-
+	database.collection(collectionName).insert(document, function(err, results) {
+		fn(err, results);
 	});
 
 };
 
 exports.readById = function(id, fn) {
 
-	mongoClient.connect('mongodb://127.0.0.1:27017/' + databaseName, function(err, db) {
-
-		db.collection(collectionName).findOne({
-			'document_id' : id
-		}, function(err, results) {
-			fn(err, results);
-		});
-
+	database.collection(collectionName).findOne({
+		'document_id' : id
+	}, function(err, results) {
+		fn(err, results);
 	});
 
 };
 
 exports.update = function(document, fn) {
 
-	mongoClient.connect('mongodb://127.0.0.1:27017/' + databaseName, function(err, db) {
-	console.log(err);
-		db.collection(collectionName).update({
-			'document_id' : document.document_id
-		}, document, function(err, results) {
-			fn(err, results);
-		});
-
+	database.collection(collectionName).update({
+		'document_id' : document.document_id
+	}, document, function(err, results) {
+		fn(err, results);
 	});
 
 };
