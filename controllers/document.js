7ca54eac048c56b857a4039e447e98ef683b2085
Alan James <ajames@Landmine.fullsail.com>
2013-11-19 21:03:52 +0100

    Commit

--- controllers/document.js
+++ controllers/document.js
@@ -3,7 +3,7 @@ module.exports.controller = function(app) {
 	app.get('/document/:document_id', function(req, res) {
 		
 		var documentId = req.params.document_id;
-		console.log(documentId)
+		console.log(documentId);
 		
 		res.render('document/index');
 	});
